019042fd992627d2e719a79dc92c76294cba06e9
Matenas, Simonas <simonas.matenas@dell.com>
2026-02-03 14:43:06 +0200

    Code review #1

--- src/Discounts.API/Controllers/DiscountsController.cs
+++ src/Discounts.API/Controllers/DiscountsController.cs
@@ -8,18 +8,18 @@ namespace Discounts.API.Controllers;
 [Route("[controller]")]
 public class DiscountsController(ILogger<DiscountsController> logger) : ControllerBase
 {
-    private readonly ILogger<DiscountsController> _logger = logger;
-
     private static readonly List<Discount> Discounts =
     [
-        new() { Id = "id1", Code = "Year2026", DiscountPercent = 5, ValidUnit = DateTime.Now.AddDays(30) },
-        new() { Id = "id2", Code = "Year2025", DiscountPercent = 4, ValidUnit = DateTime.Now.AddDays(-5) },
+        new() { Id = "id1", Code = "Year2026", DiscountPercent = 5, ValidUnit = DateTime.UtcNow.AddDays(30) },
+        new() { Id = "id2", Code = "Year2025", DiscountPercent = 4, ValidUnit = DateTime.UtcNow.AddDays(-5) },
         new() { Id = "id3", Code = "SecretDiscount2026", DiscountPercent = 15, ValidUnit = DateTime.Now.AddDays(60) }
     ];
 
     [HttpGet(Name = "GetAllDiscounts")]
     public IEnumerable<DiscountDto> GetAllDiscounts()
     {
+        logger.LogInformation("GetAllDiscounts called");
+        
         return Discounts
             .Select(x => new DiscountDto(x.Code, x.DiscountPercent))
             .ToArray();
